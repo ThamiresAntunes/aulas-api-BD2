c1328a370f3fe044c45d659fb985e4e2a00c3537
ThamiresAntunes <antunesthamires8@gmail.com>
2025-04-22 15:09:43 -0300

    apagando codigo comentado

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-/*import User from "./model/user.js";
-import Task from "./model/task.js";
-
-async function salvarUser() {
-    const user = await User.create({
-        firstName: "Jose",
-        lastName: "Silva"
-    });
-    console.log(user.toJSON());
-    console.log('User saved successfully!');
-}
-
-salvarUser();
-
-
-async function salvarTask() {
-    const task = await Task.create({
-        titulo: "task 1",
-        descricao: "task de teste para o banco",
-        tipo: "Pessoal",
-
-    });
-    console.log(task.toJSON());
-    console.log('task saved successfully!');
-}
-
-salvarTask();
-*/
-
 import express from 'express';
 import Task from './model/task.js'
+
 const app = express();
 app.use(express.json());
 

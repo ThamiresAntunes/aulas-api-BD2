73548f6d19451b1a351953a6d432d757673eae49
ThamiresAntunes <antunesthamires8@gmail.com>
2025-04-22 15:23:00 -0300

    criando o controller e refatorando o codigo já existente

---
+++ controller/task-controller.js
@@ -0,0 +1,19 @@
+import Task from '../model/task.js';
+
+export async function getTasks(req, res) {
+    const tasks = await Task.findAll();
+    res.status(200).json(tasks);
+}
+
+export async function createTask(req, res){
+    try{
+        const task = req.body;
+        const newTask = await Task.create(task);
+        res.status(201).json(newTask);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json("Erro ao criar tarefa");
+    }
+    
+}
--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 import express from 'express';
-import Task from './model/task.js'
+import { getTasks, createTask } from './controller/task-controller';
 
 const app = express();
 app.use(express.json());
 
-app.get('/tasks', async (req, res) => {
-    const tasks = await Task.findAll();
-    res.status(200).json(tasks);
-});
+app.get('/tasks', getTasks);
 
-app.post('/tasks', async (req, res) => {
-    try{
-        const task = req.body;
-        const newTask = await Task.create(task);
-        res.status(201).json(newTask);
-    }
-    catch(err){
-        console.log(err);
-        res.status(400).json("Erro ao criar tarefa");
-    }
-    
-})
+app.post('/tasks', createTask);
 
 const porta = 3000;
 
